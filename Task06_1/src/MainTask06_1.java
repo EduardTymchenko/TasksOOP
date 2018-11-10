70d6b9f7b0db134de59e3288f28090c9c145e0a4
Eduard <timchenko_eduard@ukr.net>
2018-11-10 14:15:43 +0200

    Tasks for Java SE OOP

--- Task06_1/src/MainTask06_1.java
+++ Task06_1/src/MainTask06_1.java
@@ -1,6 +1,6 @@
 public class MainTask06_1 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++){
+        for (int i = 1; i <= 100; i++){
             Thread thread = new Thread(new ThreadFactorial(i));
             thread.start();
         }
