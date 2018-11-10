6545615e51be2ee75d7a7ee8443d5ce30b97a2ba
Eduard <timchenko_eduard@ukr.net>
2018-11-10 14:12:32 +0200

    Tasks for Java SE OOP

---
+++ Task06_1/src/MainTask06_1.java
@@ -0,0 +1,8 @@
+public class MainTask06_1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++){
+            Thread thread = new Thread(new ThreadFactorial(i));
+            thread.start();
+        }
+    }
+}
---
+++ Task06_1/src/ThreadFactorial.java
@@ -0,0 +1,31 @@
+import java.math.BigInteger;
+
+public  class ThreadFactorial implements Runnable {
+    private int n;
+
+    public ThreadFactorial(int n) {
+        this.n = n;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public static BigInteger factorial(int n) {
+        if (n == 0) return BigInteger.ONE;
+        return BigInteger.valueOf(n).multiply(factorial(n - 1)) ;
+    }
+
+    @Override
+    public synchronized void run() {
+        factorial(n);
+        System.out.println("Factorial " + n + " = " + factorial(n));
+    }
+
+}
+
+
