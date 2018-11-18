3a8b0eac32ebb556a12c5bb5dc0d43e5a9102454
Eduard <timchenko_eduard@ukr.net>
2018-11-18 22:58:39 +0200

    Tasks for Java SE OOP

---
+++ Task09_4/src/MagicQueue.java
@@ -0,0 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+
+public class MagicQueue {
+
+    public static void queneView(int numberCola) {
+        Deque<String> queue = new ArrayDeque<>();
+        queue.add ("Sheldon");
+        queue.add("Leonard");
+        queue.add("Volovitc");
+        queue.add("Kutrapalli");
+        queue.add("Penny");
+        String tmp;
+        for (int i = 0; i < numberCola; i++){
+            tmp = queue.poll();
+            queue.add(tmp);
+            queue.add(tmp);
+        }
+
+        System.out.print(queue.toString());
+    }
+
+}
---
+++ Task09_4/src/MainTask09_4.java
@@ -0,0 +1,5 @@
+public class MainTask09_4 {
+    public static void main(String[] args) {
+        MagicQueue.queneView(10);
+    }
+}
