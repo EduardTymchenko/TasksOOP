3b19e272e4568d1673acc3ac725cef20be7b8ddb
Eduard <timchenko_eduard@ukr.net>
2018-11-23 09:38:04 +0200

    Tasks for Java SE OOP

---
+++ Task09StreamAPI_2/src/MainTask09SAPI_2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class MainTask09SAPI_2 {
+    public static void main(String[] args) {
+        int[] arrayInt = { 5, -7, 4, -2, 1, 5, 3, -8, -4, -1};
+        int rezPositive = Arrays.stream(arrayInt).filter(n -> n >= 0).min().getAsInt();
+        int rezNegative = Arrays.stream(arrayInt).filter(n -> n < 0).max().getAsInt();
+        System.out.println(rezNegative);
+        System.out.println(rezPositive);
+        if (rezPositive <= Math.abs(rezNegative)) System.out.println(rezPositive);
+        else System.out.println(rezNegative);
+    }
+}
