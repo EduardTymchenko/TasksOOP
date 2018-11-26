744d8428c3d06d123b82cab64416c4efdf3db128
Eduard <timchenko_eduard@ukr.net>
2018-11-27 01:25:28 +0200

    Tasks for Java SE OOP

--- Task10_3/src/MainTask103.java
+++ Task10_3/src/MainTask103.java
@@ -7,15 +7,11 @@ public class MainTask103 {
         int min = -10;
         int max = 10;
         int diff = max - min;
-        int count;
         Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < arrayInt.length; i++) {
             arrayInt[i] = random.nextInt(diff + 1);
             arrayInt[i] += min;
-            if (map.containsKey(arrayInt[i])) {
-                count = map.get(arrayInt[i]);
-                map.put(arrayInt[i],++count);
-            }
+            if (map.containsKey(arrayInt[i])) map.put(arrayInt[i],map.get(arrayInt[i])+1);
             else map.put(arrayInt[i],1);
 
         }
